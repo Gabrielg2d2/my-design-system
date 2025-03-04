acddfc86f0211fd7c4dc1512ca13153b104731fc
Gabriel <gabriel.g2d2@gmail.com>
2025-03-04 09:02:32 -0300

    Remove Storybook configuration and associated stories for cleanup

--- stories/Button.stories.jsx
+++
@@ -1,9 +0,0 @@
-import Button from "../src/components/Button";
-
-export default {
-  title: "Components/Button",
-  component: Button,
-};
-
-export const Primary = () => <Button>Primary My</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
