15c71b5f7eccafcffff9968ce800548ede6fae8a
Gabriel <gabriel.g2d2@gmail.com>
2025-03-04 10:53:57 -0300

    Add custom theme configuration for Storybook with dark mode support

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,20 @@
 import type { Preview } from "@storybook/react";
+import { themes } from "@storybook/theming";
 import "../src/global/global.css";
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
+    backgrounds: {
+      default: "Dark",
+      values: [
+        { name: "Dark", value: "#333" },
+        { name: "Light", value: "#F7F9F2" },
+        { name: "Green", value: "#228B22" },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
