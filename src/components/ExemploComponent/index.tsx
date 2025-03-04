2a1ce9b5e258f49a7ede4871cae4ed53ca57661c
Gabriel <gabriel.g2d2@gmail.com>
2025-03-04 10:01:44 -0300

    Fix class attribute to className in App and ExemploComponent

--- src/components/ExemploComponent/index.tsx
+++ src/components/ExemploComponent/index.tsx
@@ -1,7 +1,7 @@
 export function ExemploComponent() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
