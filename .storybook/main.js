80bbb89fd74b09b5902f91d2deeb80b21d497eba
Gabriel <gabriel.g2d2@gmail.com>
2025-03-03 22:39:05 -0300

    Update Storybook configuration to streamline stories definition and maintain autodocs support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,24 +1,16 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
-
+export default {
+  stories: ["../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
   ],
-
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
-
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
-export default config;
