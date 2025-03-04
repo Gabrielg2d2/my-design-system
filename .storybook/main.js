74e9c3ec996c8101e01fd9ac70bc0cdd3033e2d8
Gabriel <gabriel.g2d2@gmail.com>
2025-03-03 22:21:28 -0300

    Add autodocs support in Storybook configuration and update peer dependencies for React

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,15 +4,21 @@ const config = {
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+
+  docs: {
+    autodocs: true
+  }
 };
 export default config;
