6d28d32ec2f4a15116988c15fcb39abb87c55ab0
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 10:50:39 -0300

    Update Icon stories to use InputIcon component

--- stories/components/Inputs/Icon.stories.tsx
+++ stories/components/Inputs/Icon.stories.tsx
@@ -2,18 +2,18 @@ import { Box } from '@radix-ui/themes'
 import type { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
 import {
-	InputDefault,
-	type TInputDefaultProps,
-} from '../../../src/components/Inputs/Default'
+	InputIcon,
+	type TInputIconProps,
+} from '../../../src/components/Inputs/InputIcon'
 
 import * as RootStories from './Root.stories'
 
-const meta: Meta<TInputDefaultProps> = {
+const meta: Meta<TInputIconProps> = {
 	title: 'Inputs/Icon',
-	component: (args: TInputDefaultProps) => {
+	component: (args: TInputIconProps) => {
 		return (
 			<Box maxWidth="800px">
-				<InputDefault {...args} />
+				<InputIcon {...args} />
 			</Box>
 		)
 	},
@@ -26,6 +26,6 @@ const meta: Meta<TInputDefaultProps> = {
 }
 
 export default meta
-type Story = StoryObj<TInputDefaultProps>
+type Story = StoryObj<TInputIconProps>
 
 export const Primary: Story = {}
