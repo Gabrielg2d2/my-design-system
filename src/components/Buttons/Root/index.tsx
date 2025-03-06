9c5572197adcd1d5d9d4199081a4dae7a86d41ed
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 16:42:30 -0300

    Enhance ButtonRoot component to render children within the Radix UI Button

--- src/components/Buttons/Root/index.tsx
+++ src/components/Buttons/Root/index.tsx
@@ -3,5 +3,9 @@ import { Button, type ButtonProps } from '@radix-ui/themes'
 export type TButtonProps = ButtonProps
 
 export function ButtonRoot(props: TButtonProps) {
-	return <Button size="1" variant="soft" {...props} />
+	return (
+		<Button size="1" variant="soft" {...props}>
+			{props.children}
+		</Button>
+	)
 }
