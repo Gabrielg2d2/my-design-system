20d81c3cd0710a7a9387d46aad654c6716eccdfd
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 10:49:16 -0300

    Add Icon stories for InputDefault component

---
+++ stories/components/Inputs/Icon.stories.tsx
@@ -0,0 +1,31 @@
+import { Box } from '@radix-ui/themes'
+import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import {
+	InputDefault,
+	type TInputDefaultProps,
+} from '../../../src/components/Inputs/Default'
+
+import * as RootStories from './Root.stories'
+
+const meta: Meta<TInputDefaultProps> = {
+	title: 'Inputs/Icon',
+	component: (args: TInputDefaultProps) => {
+		return (
+			<Box maxWidth="800px">
+				<InputDefault {...args} />
+			</Box>
+		)
+	},
+	argTypes: {
+		...RootStories.Default.argTypes,
+	},
+	args: {
+		...RootStories.Default.args,
+	},
+}
+
+export default meta
+type Story = StoryObj<TInputDefaultProps>
+
+export const Primary: Story = {}
