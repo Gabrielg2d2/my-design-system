9cd8cec8862e46a4619cbf25fdd7ad828344758b
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 11:24:57 -0300

    Enhance InputIcon component to support left and right icons

--- src/components/Inputs/InputIcon/index.tsx
+++ src/components/Inputs/InputIcon/index.tsx
@@ -1,7 +1,17 @@
+import { TextField } from '@radix-ui/themes'
 import { InputRoot, type TInputRootProps } from '../Root'
 
-export type TInputIconProps = TInputRootProps
+export type TInputIconProps = TInputRootProps & {
+	iconLeft?: React.ReactNode
+	iconRight?: React.ReactNode
+}
 
 export function InputIcon(props: TInputIconProps) {
-	return <InputRoot {...props} />
+	return (
+		<InputRoot {...props}>
+			{!props.iconLeft && props.iconRight && <TextField.Slot />}
+			{props.iconLeft && <TextField.Slot>{props.iconLeft}</TextField.Slot>}
+			{props.iconRight && <TextField.Slot>{props.iconRight}</TextField.Slot>}
+		</InputRoot>
+	)
 }
--- stories/components/Inputs/Icon.stories.tsx
+++ stories/components/Inputs/Icon.stories.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@radix-ui/themes'
+import { Box, IconButton } from '@radix-ui/themes'
 import type { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
 import {
@@ -6,6 +6,7 @@ import {
 	type TInputIconProps,
 } from '../../../src/components/Inputs/InputIcon'
 
+import { DotsHorizontalIcon, MagnifyingGlassIcon } from '@radix-ui/react-icons'
 import * as RootStories from './Root.stories'
 
 const meta: Meta<TInputIconProps> = {
@@ -29,3 +30,35 @@ export default meta
 type Story = StoryObj<TInputIconProps>
 
 export const Primary: Story = {}
+
+export const IconLeft: Story = {
+	args: {
+		iconLeft: <MagnifyingGlassIcon height="14" width="14" />,
+	},
+}
+
+export const IconRight: Story = {
+	args: {
+		iconRight: <MagnifyingGlassIcon height="14" width="14" />,
+	},
+}
+
+export const IconButtonLeft: Story = {
+	args: {
+		iconLeft: (
+			<IconButton size="1" variant="ghost">
+				<MagnifyingGlassIcon height="14" width="14" />
+			</IconButton>
+		),
+	},
+}
+
+export const IconButtonRight: Story = {
+	args: {
+		iconRight: (
+			<IconButton size="1" variant="ghost">
+				<MagnifyingGlassIcon height="14" width="14" />
+			</IconButton>
+		),
+	},
+}
