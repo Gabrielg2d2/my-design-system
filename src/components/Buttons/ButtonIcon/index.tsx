47384212c7d782f1cbefb219495aec39c06cc346
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 17:29:10 -0300

    Add Spinner and BookmarkIcon to ButtonIcon component; create ButtonIcon stories

--- src/components/Buttons/ButtonIcon/index.tsx
+++ src/components/Buttons/ButtonIcon/index.tsx
@@ -1,3 +1,5 @@
+import { BookmarkIcon } from '@radix-ui/react-icons'
+import { Spinner } from '@radix-ui/themes'
 import { ButtonRoot, type TButtonRootProps } from '../Root'
 
 export type TButtonIconProps = TButtonRootProps & {
@@ -8,6 +10,9 @@ export type TButtonIconProps = TButtonRootProps & {
 export function ButtonIcon(props: TButtonIconProps) {
 	return (
 		<ButtonRoot size="3" variant="classic" {...props}>
+			<Spinner loading>
+				<BookmarkIcon />
+			</Spinner>
 			{props.children}
 		</ButtonRoot>
 	)
---
+++ stories/components/Buttons/ButtonIcon.stories.tsx
@@ -0,0 +1,27 @@
+import { Box } from '@radix-ui/themes'
+import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import {
+	ButtonIcon,
+	type TButtonIconProps,
+} from '../../../src/components/Buttons/ButtonIcon'
+
+import * as RootStories from './Root.stories'
+
+const meta: Meta<TButtonIconProps> = {
+	title: 'Buttons/ButtonIcon',
+	component: (args: TButtonIconProps) => {
+		return <ButtonIcon {...args}>Text Button</ButtonIcon>
+	},
+	argTypes: {
+		...RootStories.Default.argTypes,
+	},
+	args: {
+		...RootStories.Default.args,
+	},
+}
+
+export default meta
+type Story = StoryObj<TButtonIconProps>
+
+export const Default: Story = {}
