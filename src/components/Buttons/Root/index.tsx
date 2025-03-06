d964cf784ba51df79fb2a8b1a7517adae3bcb5b4
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 17:55:52 -0300

    Add fullWidth, width, and maxWidth props to ButtonRoot component

--- src/components/Buttons/Root/index.tsx
+++ src/components/Buttons/Root/index.tsx
@@ -1,10 +1,22 @@
 import { Button, type ButtonProps } from '@radix-ui/themes'
 
-export type TButtonRootProps = ButtonProps
+export type TButtonRootProps = ButtonProps & {
+	fullWidth?: boolean
+	width?: string
+	maxWidth?: string
+}
 
 export function ButtonRoot(props: TButtonRootProps) {
 	return (
-		<Button size="3" variant="classic" {...props}>
+		<Button
+			size="3"
+			variant="classic"
+			style={{
+				width: props.fullWidth ? '100%' : props.width,
+				maxWidth: props.maxWidth,
+			}}
+			{...props}
+		>
 			{props.children}
 		</Button>
 	)
