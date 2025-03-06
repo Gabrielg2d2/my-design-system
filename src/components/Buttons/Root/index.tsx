ae35b39bd5793b276d5867a657679e56c73c96a0
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 16:38:27 -0300

    Add ButtonRoot component to encapsulate Radix UI Button with default props

---
+++ src/components/Buttons/Root/index.tsx
@@ -0,0 +1,7 @@
+import { Button, type ButtonProps } from '@radix-ui/themes'
+
+export type TButtonProps = ButtonProps
+
+export function ButtonRoot(props: TButtonProps) {
+	return <Button size="1" variant="soft" {...props} />
+}
