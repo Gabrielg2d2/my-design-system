f8c65e426f8f4fcb15443d1019954010dd68772c
Gabriel <gabriel.g2d2@gmail.com>
2025-03-06 10:44:51 -0300

    Add InputDefault component that wraps InputRoot

---
+++ src/components/Inputs/InputIcon/index.tsx
@@ -0,0 +1,7 @@
+import { InputRoot, type TInputRootProps } from '../Root'
+
+export type TInputIconProps = TInputRootProps
+
+export function InputIcon(props: TInputIconProps) {
+	return <InputRoot {...props} />
+}
