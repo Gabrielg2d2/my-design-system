58c87656f5bf99101326a9a5cdcfd3fcfcf308c2
Gabriel <gabriel.g2d2@gmail.com>
2025-03-12 15:48:26 -0300

    Refactor ButtonIcon to use iconName props for dynamic icon rendering

--- src/components/Buttons/ButtonIcon/index.tsx
+++ src/components/Buttons/ButtonIcon/index.tsx
@@ -1,21 +1,39 @@
+import * as Icons from '@radix-ui/react-icons'
 import { Spinner } from '@radix-ui/themes'
 import { ButtonRoot, type TButtonRootProps } from '../Root'
 
+type NamesIcon = keyof typeof Icons
+
 export type TButtonIconProps = TButtonRootProps & {
-	iconLeft?: React.ReactNode
-	iconRight?: React.ReactNode
+	iconLeftName?: NamesIcon
+	iconRightName?: NamesIcon
 	loadingIcon?: boolean
 }
 
 export function ButtonIcon(props: TButtonIconProps) {
+	if (!props.iconLeftName && !props.iconRightName) {
+		throw new Error('You must provide either iconLeftName or iconRightName')
+	}
+
+	const IconComponentLeft = props.iconLeftName
+		? Icons[props.iconLeftName]
+		: null
+	const IconComponentRight = props.iconRightName
+		? Icons[props.iconRightName]
+		: null
+
 	return (
 		<ButtonRoot size="3" variant="classic" {...props}>
-			{props.iconLeft && (
-				<Spinner loading={props.loadingIcon}>{props.iconLeft}</Spinner>
+			{props.iconLeftName && (
+				<Spinner loading={props.loadingIcon}>
+					{IconComponentLeft && <IconComponentLeft />}
+				</Spinner>
 			)}
 			{props.children}
-			{props.iconRight && (
-				<Spinner loading={props.loadingIcon}>{props.iconRight}</Spinner>
+			{props.iconRightName && (
+				<Spinner loading={props.loadingIcon}>
+					{IconComponentRight && <IconComponentRight />}
+				</Spinner>
 			)}
 		</ButtonRoot>
 	)
