61c230761a1d5559b998f42707801e001e2cb412
Gabriel <gabriel.g2d2@gmail.com>
2025-03-19 16:52:08 -0300

    Refactor TextCustom component to use local type alias for props

--- src/components/Typography/Text/Custom/index.tsx
+++ src/components/Typography/Text/Custom/index.tsx
@@ -2,7 +2,7 @@ import { type TTextRootProps, TextRoot } from '../Root'
 
 type TProps = TTextRootProps
 
-export type TTextCustomProps = TTextRootProps
+export type TTextCustomProps = TProps
 
 export function TextCustom(props: TProps) {
 	return <TextRoot {...props}>{props.children}</TextRoot>
