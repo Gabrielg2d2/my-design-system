d7cc79621e35d39bbb152ffb1edb7adaa27fce67
Gabriel <gabriel.g2d2@gmail.com>
2025-03-19 17:42:54 -0300

    Add additional controls for Root component stories in Storybook

--- stories/components/Typography/Text/Root.stories.tsx
+++ stories/components/Typography/Text/Root.stories.tsx
@@ -30,5 +30,44 @@ export const Default: Story = {
 				options: ['sm', 'md', 'lg'],
 			},
 		},
+		highContrast: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		weight: {
+			control: {
+				type: 'select',
+				options: ['regular', 'medium', 'bold'],
+			},
+		},
+		align: {
+			control: {
+				type: 'select',
+				options: ['left', 'center', 'right'],
+			},
+		},
+		trim: {
+			control: {
+				type: 'select',
+				options: ['normal', 'both'],
+			},
+		},
+		truncate: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		wrap: {
+			control: {
+				type: 'boolean',
+			},
+		},
+		as: {
+			control: {
+				type: 'select',
+				options: ['p', 'span', 'div', 'label'],
+			},
+		},
 	},
 }
